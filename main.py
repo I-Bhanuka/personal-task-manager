51fa01038090b2f861da7296d7d792c6ab8c844a
Isith Bhanuka <bhanukaappuhamy@gmail.com>
2025-05-01 21:44:30 +0530

    Phase 1 - CLI version setup

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Personal Expense Tracker")
+def main():
+    print("Hello, personal expense tracker!")
+
+
+def add_expense():
+    "This function adds expenses"
+    pass
+
+def view_expense():
+    "This function displays the expenses"
+    pass
+
+def monthly_summary():
+    "This function displays a monthly summary"
+    pass
+
+
+if __name__ == "__main__":
+    main()
